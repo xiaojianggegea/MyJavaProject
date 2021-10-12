38e2662343b120c3932d669c740752671a4abbb0
xiaojianggegea <2314398223@qq.com>
2021-10-12 16:18:09 +0800

    学生类

---
+++ mybatis/mybatis-01/src/main/java/com/ghj/entity/Student.java
@@ -0,0 +1,66 @@
+package com.ghj.entity;
+
+import java.sql.Date;
+
+/**
+ * @program: mybatis
+ * @description: 学生类
+ * @author: 小江
+ * @create: 2021-10-12 16:12
+ **/
+
+public class Student {
+    private int id;
+    private String name;
+    private String email;
+    private Date dob;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public Student(int id, String name, String email, Date dob) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                '}';
+    }
+}
