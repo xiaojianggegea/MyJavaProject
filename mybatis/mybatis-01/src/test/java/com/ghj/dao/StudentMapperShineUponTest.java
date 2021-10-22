7d255c8e8f7320293804b8a6c8d1e36eebe78c2b
xiaojianggegea <2314398223@qq.com>
2021-10-22 14:56:05 +0800

    StudentManyToMany完成

--- mybatis/mybatis-01/src/test/java/com/ghj/dao/StudentMapperShineUponTest.java
+++ mybatis/mybatis-01/src/test/java/com/ghj/dao/StudentMapperShineUponTest.java
@@ -45,7 +45,7 @@ public class StudentMapperShineUponTest {
         SqlSession sqlSession = MybatisUtil.getSqlSession();
         StudentManyToManyMapper studentManyToManyMapper =
                 sqlSession.getMapper(StudentManyToManyMapper.class);
-        StudentManyToMany studentManyToMany = new StudentManyToMany("张三","男","计算机","大四");
+        StudentManyToMany studentManyToMany = new StudentManyToMany("张三", "男", "计算机", "大四");
         int n = studentManyToManyMapper.insertStudent(studentManyToMany);
         if (n > 0) {
             System.out.println("插入成功！");
@@ -62,8 +62,8 @@ public class StudentMapperShineUponTest {
         SqlSession sqlSession = MybatisUtil.getSqlSession();
         StudentManyToManyMapper studentManyToManyMapper =
                 sqlSession.getMapper(StudentManyToManyMapper.class);
-        Course course1 = new Course("001","corejava");
-        Course course2 = new Course("002","oracle");
+        Course course1 = new Course("001", "corejava");
+        Course course2 = new Course("002", "oracle");
         studentManyToManyMapper.insertCourse(course1);
         int n = studentManyToManyMapper.insertCourse(course2);
         if (n > 0) {
@@ -75,24 +75,57 @@ public class StudentMapperShineUponTest {
         sqlSession.close();
     }
 
-    //    多对多插入数据
+    //    多对多通过ID查询学生数据
     @Test
     public void test05() {
         SqlSession sqlSession = MybatisUtil.getSqlSession();
+        StudentManyToManyMapper studentManyToManyMapper =
+                sqlSession.getMapper(StudentManyToManyMapper.class);
+        StudentManyToMany studentManyToMany = studentManyToManyMapper.getStudentById(21);
+        System.out.println(studentManyToMany);
 
     }
 
-    //    多对多插入数据
+    //    多对多通过ID查询课程数据
     @Test
     public void test06() {
         SqlSession sqlSession = MybatisUtil.getSqlSession();
-
+        StudentManyToManyMapper studentManyToManyMapper =
+                sqlSession.getMapper(StudentManyToManyMapper.class);
+        Course course = studentManyToManyMapper.getCourseById(22);
+        System.out.println(course);
     }
 
     //    多对多插入数据
     @Test
     public void test07() {
         SqlSession sqlSession = MybatisUtil.getSqlSession();
+        StudentManyToManyMapper studentManyToManyMapper =
+                sqlSession.getMapper(StudentManyToManyMapper.class);
+        StudentManyToMany studentManyToMany = studentManyToManyMapper.getStudentById(21);
+        Course course = studentManyToManyMapper.getCourseById(22);
+        studentManyToManyMapper.studentSelectCourse(studentManyToMany, course);
+        sqlSession.commit();
+        sqlSession.close();
+    }
 
+    //    查询比指定ID小的信息
+    @Test
+    public void test08() {
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
+        StudentManyToManyMapper studentManyToManyMapper =
+                sqlSession.getMapper(StudentManyToManyMapper.class);
+        List<StudentManyToMany> list = studentManyToManyMapper.getStudentByIdOnCondition(23);
+        list.forEach(System.out::println);
+    }
+
+    //    查询student级联查询出所选的course并且组装成完整的对象
+    @Test
+    public void test09() {
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
+        StudentManyToManyMapper studentManyToManyMapper =
+                sqlSession.getMapper(StudentManyToManyMapper.class);
+        StudentManyToMany studentManyToMany = studentManyToManyMapper.getStudentByIdWithCourses(21);
+        System.out.println(studentManyToMany);
     }
 }
